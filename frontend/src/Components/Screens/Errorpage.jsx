6f3a00ee546b5907f1f883444aa38ff0ada147e3
faizan alam <alamf6023@gmail.com>
2023-07-31 15:26:06 +0530

    update: improve 404 page and complete it with designing

--- frontend/src/Components/Screens/Errorpage.jsx
+++ frontend/src/Components/Screens/Errorpage.jsx
@@ -1,26 +1,42 @@
 import { gsap } from 'gsap';
 import { Draggable } from 'gsap/Draggable';
 import { useEffect } from 'react';
+import LangList from '../LangList';
+import headerImg from '../../assets/favi-removebg-preview.png'
+import { NavLink } from 'react-router-dom';
 gsap.registerPlugin(Draggable);
+
 
 function Errorpage() {
         useEffect(() => {
-          Draggable.create("#box1", { type: "x,y", edgeResistance: 0.65,bounds: {right:5,left:10,top:5,bottom:5} });
-          Draggable.create("#box2", { type: "x,y", edgeResistance: 0.65,bounds: {right:7,left:10,top:7,bottom:7} });
-          Draggable.create("#box3", { type: "x,y", edgeResistance: 0.65,bounds: {right:10,left:10,top:10,bottom:10}});
+          Draggable.create("#dragg", { type: "x,y", edgeResistance: 0.65,bounds: {left:50,top:50}});
         }, []);
   return (
-    <div className='errorcontainer'>
-    <div className="boxes">
-
-        <div id="box1" className="box" style={{backgroundColor: 'red'}}>JavaScript Compiler </div>
-        <div id="box2" className="box" style={{backgroundColor: 'blue'}}>Python Compiler</div>
-        <div id="box3" className="box" style={{backgroundColor: 'green'}}>Java Compiler</div>
-        <div id="box3" className="box" style={{backgroundColor: 'green'}}>Website Editor</div>
-    </div>
-    </div>
+  <div className="errorcontainer">
+  <div className="errorHeader">
+      {/* <Header/> */}
+      <div className="errorheader1">
+            <img className='errortopLogo' src={headerImg} alt="MainLogo" /><p>Codo File</p>
+        </div>
+  </div>
+  <div className="errorBody">
+      <div className="leftLang">
+          <LangList/>
+      </div>
+      <div className="errorPage" >
+          <div className="errortextcontainer" id='dragg'>
+          <div className="messageEr">
+            <h1>404</h1><h3 className='eerrrroorr'>The page you are looking for is out to <span style={{fontSize:"2rem",color:"gray"}}> lunch.</span></h3>
+            <h3>Please come back later.</h3>
+          </div><br />
+            <NavLink to='/'><button className='btn'>Back to Home</button></NavLink>
+          </div>
+      </div>
+  </div>
+</div>
   )
 }
 
 export default Errorpage
+
 
