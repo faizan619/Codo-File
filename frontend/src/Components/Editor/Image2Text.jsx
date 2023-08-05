b3048edb0f5362af639092204f3f31bdc461c277
faizan alam <alamf6023@gmail.com>
2023-08-05 22:02:57 +0530

    build:created image to text feature --complete working

--- frontend/src/Components/Editor/Image2Text.jsx
+++ frontend/src/Components/Editor/Image2Text.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LangList from './LangList'
+import Tesseract from 'tesseract.js';
 
 function Image2Text() {
+
+  const [imagePath,setImagePath] = useState("/mainLogo.png");
+  const [text,setText] = useState("");
+
+  const handleChange = (event) => {
+    setImagePath(URL.createObjectURL(event.target.files[0]));
+  }
+
+  const handleClick = () => {
+  
+    Tesseract.recognize(
+      imagePath,'eng',
+      { 
+        logger: m => console.log(m) 
+      }
+    )
+    .catch (err => {
+      console.error(err);
+    })
+    .then(result => {
+      // Get Confidence score
+      let confidence = result.data.confidence
+     
+      let text = result.data.text
+      setText(text);
+      console.log(confidence,text);
+  
+    })
+  }
+
   return (
     <>
       <div className="voiceContainer">
@@ -10,6 +41,16 @@ function Image2Text() {
                     <LangList/>
                 </div>
                 <div className="voicePlayground">
+                <mark><h1>Image to Text Converter</h1></mark><br />
+                  <main className='imageMain'>
+                    <img src={imagePath} className='Image-Logo' alt="logo" />
+                    <input className='imagefiletype' type="file" onChange={handleChange} />
+                    <button onClick={handleClick} className='btn'>Convert To Text</button>
+                    
+                    <div className="image-text-box">
+                      <p>{text}</p>
+                    </div>
+                  </main>
                 </div>
             </div>
         </div>
