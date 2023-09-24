58952893f7f95147e4771d8f0276b4a36c8a53ef
faizan alam <alamf6023@gmail.com>
2023-09-24 13:22:36 +0530

    add certificate section

--- frontend/src/Components/Screens/Homepage.jsx
+++ frontend/src/Components/Screens/Homepage.jsx
@@ -8,7 +8,8 @@ import Pycod from '../../assets/Man reading-pana.svg'
 import SpeechCod from '../../assets/Speech to text-bro.svg'
 import CodJava from '../../assets/Coding-bro.svg'
 import ImagCod from '../../assets/Writing on the wall-rafiki.svg'
-import MoreCod from '../../assets/Sponsor-rafiki.svg';
+// import MoreCod from '../../assets/Sponsor-rafiki.svg';
+import MoreCod from '../../assets/paralympic winners podium-pana.svg';
 import Feedback from '../HomepageScreen/Feedback';
 import Footer from '../HomepageScreen/Footer';
 
@@ -26,6 +27,7 @@ function Homepage() {
         <LandingPage/> 
         <CodingPage 
           title="JavaScript Compiler"
+          con="Run JavaScript"
           info={<>All The <mark>Logic</mark> That you need to Learn and Practice <mark> JavaScript </mark> will be Accomplished by this <mark> Js Text Editor </mark> .</>} 
           path='/editor/javaScript' 
           image={ImageCod}
@@ -34,6 +36,7 @@ function Homepage() {
         />
         <CodingPage
           title="Python Compiler"
+          con="Run Python"
           path="/editor/python"
           info={<>Leash out All your <mark> Logic and Understanding</mark> with the Easiest Programming Language <mark>Python</mark> in this Super Easy Web IDE</>}
           image={Pycod}
@@ -43,6 +46,7 @@ function Homepage() {
         <CodingPage
           title="Dart Compiler"
           path='/editor/java'
+          con="Run Dart"
           info={<><mark>Dart</mark> is a Programming Language <mark>Developed by Google</mark> and it is use with <mark>Flutter </mark>to Create Mobile and Web Application .Embrace your <mark>Knowledge</mark> with <mark>Dart Code Editor</mark></>}
           image={CodJava}
           // poss={{'flexDirection':'row'}}
@@ -50,6 +54,7 @@ function Homepage() {
         <CodingPage
           title="Real-Time Website Editor"
           path="/editor/html"
+          con="Try Web Editor"
           info={<>
             Bored Writing HTML codes in Editor then again and again refreshing Browser for Output?<br/>
             <mark>Try</mark> our <mark>Real Time Browser</mark> that Automatically updates the Page as per your Code/Program.
@@ -62,6 +67,7 @@ function Homepage() {
           title="Image To Code"
           path='/editor/image2text'
           image={ImagCod}
+          con="Get Started"
           info={<>
             Turn Your <mark>Image into Reality</mark> with this amazing Feature of <mark>Image to Code</mark> Tool. 
           </>}
@@ -74,17 +80,22 @@ function Homepage() {
           info={<>
             <mark>"Words Speak more than Actions"</mark> let this quote get Install into your life by our latest tool <mark>Voice to Text</mark> feature .
           </>}
+          con="Get Started"
           // poss={{'flexDirection':'row-reverse'}}
           uniId={"uni"}
         />
         <CodingPage
-          title="What's More?"
-          info={<>A <mark>Programmer</mark> has no Limitation towards <mark>Learning and Improving</mark> the World Through its Skills and Knowledge</>}
+          title="What We Earned?"
+          path="https://drive.google.com/file/d/1wynjrv73vmnbzIsonh8dfJy_SweR3sq1/view?usp=sharing"
+          // info={<>A <mark>Programmer</mark> has no Limitation towards <mark>Learning and Improving</mark> the World Through its Skills and Knowledge</>}
+          info={<>Achieved <mark><u>First</u></mark> position in <mark>Website Competition</mark> held in <mark>L.N College - Borivali(East).</mark> </>}
+          con="View Certificate"
           image={MoreCod}
           // poss={{'flexDirection':'row'}}
         />
         <Feedback/>
         <Footer/>
+        {/* <a href="http://" target="_blank" rel="noopener noreferrer"></a> */}
     </div>
     </>
   )
